2df03b5282d63d6f87d155dddd0d5fbe6715b602
DanLeigh33 <vyv20001@byui.edu>
2023-01-06 19:28:49 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const conDB = require('./db/connect.js');
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 conDB.connectDB();
 
