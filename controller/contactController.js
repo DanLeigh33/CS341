e471f76e198de5e3cc60fca382b4625518db2707
DanLeigh33 <vyv20001@byui.edu>
2023-01-09 16:08:00 +0200

    Added POST, PUT and DELETE

--- controller/contactController.js
+++ controller/contactController.js
@@ -18,4 +18,55 @@ const getContact = async (req, res) => {
     });
   };
 
-  module.exports = {getDB, getContact};
+const createContact = async (req, res) => {
+  //create object to send to the collection
+  const newContact = {
+    firstName : req.body.firstName,
+    lastName : req.body.lastName,
+    email : req.body.email,
+    favoriteColor : req.body.favoriteColor,
+    birthday : req.body.birthday
+  };
+
+  
+  const resp = await mongodb.getDatabase().db('Contactss').collection('Contacts').insertOne(newContact);
+  if (resp.acknowledged) {
+    res.status(201).json(resp);
+  } else {
+    res.status(500).json(resp.error || 'Task could not be performed');
+  }
+};
+
+const updateContact = async (req, res) => {
+  //set object id to be updated
+  const id = new ObjectId(req.params.id);
+  //create new object to be sent for update
+  const newContact = {
+    firstName : req.body.firstName,
+    lastName : req.body.lastName,
+    email : req.body.email,
+    favoriteColor : req.body.favoriteColor,
+    birthday : req.body.birthday
+  };
+
+  const resp = await mongodb.getDatabase().db('Contactss').collection('Contacts').replaceOne({_id: id}, newContact);
+
+  if (resp.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(resp.error || 'Task could not be performed');
+}
+}
+
+const deleteContact = async (req, res) => {
+  const id = new ObjectId(req.params.id);
+
+  const resp = await mongodb.getDatabase().db('Contactss').collection('Contacts').deleteOne({_id: id});
+  if (resp.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(resp.error || 'Task could not be performed');
+}
+}
+
+  module.exports = {getDB, getContact, createContact, updateContact, deleteContact};
--- routes/contacts.js
+++ routes/contacts.js
@@ -7,4 +7,10 @@ router.get('/', contact.getDB);
 
 router.get('/:id', contact.getContact);
 
+router.post('/', contact.createContact);
+
+router.put('/:id', contact.updateContact);
+
+router.delete('/:id', contact.deleteContact);
+
 module.exports = router;
