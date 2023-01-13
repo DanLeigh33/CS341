d487d0ecdbb8eaaf332858561852c08ed0124415
DanLeigh33 <vyv20001@byui.edu>
2023-01-13 08:54:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const port = process.env.PORT || 3000;
 conDB.connectDB();
 
 app.use(bodyParser.json());
-//app.use((req, res) =>{
-  //res.setHeader('Access-Control-Allow-Origin', '*');
-//});
+app.use((req, res) =>{
+  res.setHeader('Access-Control-Allow-Origin', '*');
+});
 app.use('/', require('./routes'));
 
 
